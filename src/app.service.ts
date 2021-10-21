77b6f0f1912f8a120ce8011def817bc4cb1316d3
hhaluk <huseyinnurbaki@gmail.com>
2021-10-21 22:06:28 +0300

    Add: shutdown logic

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Subject } from 'rxjs';
 
 @Injectable()
 export class AppService {
+  private exitCode = 0;
+  private readonly logger = new Logger(AppService.name);
+
+  private shutdownListener$: Subject<void> = new Subject();
+
+  subscribeToShutdown(shutdownFn: (code) => void): void {
+    this.shutdownListener$.subscribe(() => shutdownFn(this.exitCode));
+  }
+
+  foo() {
+    this.logger.log('Job running..');
+    this.shutdownListener$.next();
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppService } from './app.service';
 
 async function bootstrap() {
+  const isCronJob = process.env.CRONJOB === 'CRONJOB';
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(3000, () => {
+    if (isCronJob) {
+      const statusJob = app.get(AppService);
+      statusJob.subscribeToShutdown(async (code) => {
+        await app.close();
+        console.log('Process exiting...');
+        process.exit(code || 0);
+      });
+      statusJob.foo();
+    }
+  });
 }
 bootstrap();
